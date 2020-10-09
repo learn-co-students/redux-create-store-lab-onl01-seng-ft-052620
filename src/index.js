156b755d79b72821e81982c8a4b61cb1a09758ea
ejguzman3988 <eriberto.guzman@outlook.com>
2020-10-09 16:08:40 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 // write your createStore function here
 
+function createStore(reducer){
+  let state; 
+  function dispatch(action){
+    state = reducer(state, action)
+    render()
+  }
+
+  function getState(){
+    return state
+  }
+
+  return { dispatch, getState }
+}
+
 function candyReducer(state = [], action) {
   switch (action.type) {
     case 'ADD_CANDY':
@@ -8,7 +22,8 @@ function candyReducer(state = [], action) {
       return state;
   }
 }
-
+let store = createStore(candyReducer)
+store.dispatch({type: '@@INIT'})
 function render() {
   let container = document.getElementById('container');
   if(store.getState()) {
