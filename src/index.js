cbf1e0a1d8e1c417424d1db83c11c716971c21d7
nae <nae@JenChrisensiMac.attlocal.net>
2020-10-09 12:42:11 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-// write your createStore function here
+export default function createStore(reducer) {
+  let state;
+  function dispatch(action) {
+    state = reducer(state, action)
+    render()
+  }
 
+  function getState() {
+    return state;
+  }
+
+  dispatch({ type: '@@INIT' })
+  return {dispatch,  getState}
+function render() {
+    let container = document.getElementById('container');
+}
+}
 function candyReducer(state = [], action) {
   switch (action.type) {
     case 'ADD_CANDY':
@@ -8,15 +23,3 @@ function candyReducer(state = [], action) {
       return state;
   }
 }
-
-function render() {
-  let container = document.getElementById('container');
-  if(store.getState()) {
-    container.textContent = store.getState().join(' ')
-  } else {
-    throw new Error("the store's state has not been defined yet")
-  }
-};
-
-// Use your createStore function and the functions provided here to create a store.
-// Once the store is created, call an initial dispatch.
